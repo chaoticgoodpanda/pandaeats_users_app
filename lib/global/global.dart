cc4f3802daadbd8ad860c3ba2a2b86f259e42068
chaoticgoodpanda <lwinm@seas.upenn.edu>
2022-05-04 22:34:29 +0200

    Added google-services.json

---
+++ lib/global/global.dart
@@ -0,0 +1,6 @@
+// defining variables for use by other classes
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+SharedPreferences? sharedPreferences;
+FirebaseAuth firebaseAuth = FirebaseAuth.instance;
